091b9a7bf7150bc444c114069e70c44124b1b2ff
Salazers <leonardodanielsalazar2001@gmail.com>
2023-05-17 18:11:16 +0100

    Update tabuleiro.py

--- tabuleiro.py
+++ tabuleiro.py
@@ -1,23 +1,21 @@
-#NAO APARECE NADA DO FICHEIRO DAS REGRAS, NEM NOMES
-import os
 import tkinter as tk
 import random
-import regras as re
 
-def tab(jogadores):
-    print (jogadores)
+def tab(jogador1, jogador2):
+    print(jogador1, jogador2)
     botao_bastao = None
 
-    def hist_poicoes(row, col):
+    def hist_posicoes(row, col, jogador):
         print("Posicao celula clicada:", row, col)
+        print("Jogador:", jogador)
 
     def rodar_batoes():
         nonlocal botao_bastao
         bastoes = [random.choice(["Branco", "Preto"]) for _ in range(4)]
-    
+
         contador_branco = bastoes.count("Branco")
         contador_preto = bastoes.count("Preto")
-    
+
         label_branco.config(text=f"Branco: {contador_branco}")
         label_preto.config(text=f"Preto: {contador_preto}")
 
@@ -27,10 +25,10 @@ def tab(jogadores):
         label_branco.config(font=("Arial", 14))
         label_preto.config(font=("Arial", 14))
         botao_bastao.config(font=("Arial", 14))
-        
-#    def atualizar_nomes_jogadores():
-#        jogador1_label.config(text=jogador1)
-#        jogador2_label.config(text=jogador2)
+
+    def atualizar_nomes_jogadores():
+        jogador1_label.config(text=jogador1)
+        jogador2_label.config(text=jogador2)
 
     window = tk.Tk()
     window.geometry("800x400")
@@ -43,27 +41,45 @@ def tab(jogadores):
     for row in range(3):
         row_cells = []
         for col in range(10):
-            cell = tk.Button(board, text="", width=10, height=5, command = lambda r = row, c = col: hist_poicoes(r, c))
-            if (row + col) % 2 == 0:
-                cell.configure(bg='#8B4513')
+            if row == 0:
+                if (col + 1) % 2 != 0:
+                    cell = tk.Button(board, text="", width=10, height=5, command=lambda r=row, c=col: hist_posicoes(r, c, jogador1))
+                    cell.configure(bg='#D2B48C')
+                    cell.grid(row=row, column=col)
+                    row_cells.append(cell)
+                else:
+                    cell = tk.Button(board, text="", width=10, height=5, command=lambda r=row, c=col: hist_posicoes(r, c, jogador2))
+                    cell.configure(bg='#8B4513')
+                    cell.grid(row=row, column=col)
+                    row_cells.append(cell)
             else:
-                cell.configure(bg='#D2B48C')
-            cell.grid(row=row, column=col)
-            row_cells.append(cell)
+                cell = tk.Button(board, text="", width=10, height=5, command=lambda r=row, c=col: hist_posicoes(r, c, ""))
+                if (row + col) % 2 == 0:
+                    cell.configure(bg='#8B4513')
+                else:
+                    cell.configure(bg='#D2B48C')
+                cell.grid(row=row, column=col)
+                row_cells.append(cell)
         cells.append(row_cells)
 
-    white_piece = tk.PhotoImage(file = "white_piece.png")
-    black_piece = tk.PhotoImage(file = "black_piece.png")
+    white_piece = tk.PhotoImage(file="white_piece.png")
+    black_piece = tk.PhotoImage(file="black_piece.png")
 
-    for row in range(3):
-        for col in range(10):
-            if (row + col) % 2 != 0:
-                cells[row][col].config(image= white_piece if row == 0 else black_piece)
-            
+    cells[0][0].config(image=white_piece)
+    cells[0][2].config(image=white_piece)
+    cells[0][4].config(image=white_piece)
+    cells[0][6].config(image=white_piece)
+    cells[0][8].config(image=white_piece)
+
+    cells[0][1].config(image=black_piece)
+    cells[0][3].config(image=black_piece)
+    cells[0][5].config(image=black_piece)
+    cells[0][7].config(image=black_piece)
+    cells[0][9].config(image=black_piece)
+
     tabuleiro = tk.Frame(window, bg='White')
     tabuleiro.pack(fill=tk.BOTH, expand=True)
 
-    # Jogadores e função de rodar bastões
     jogadores_e_bastoes = tk.Frame(window)
     jogadores_e_bastoes.pack()
 
@@ -72,8 +88,6 @@ def tab(jogadores):
 
     jogador2_label = tk.Label(jogadores_e_bastoes, text="Jogador 2")
     jogador2_label.pack(side=tk.RIGHT)
-
-#    atualizar_nomes_jogadores()
 
     counter_frame = tk.Frame(window)
     counter_frame.pack()
@@ -85,11 +99,10 @@ def tab(jogadores):
     label_preto.pack(side=tk.LEFT, padx=5)
 
     botao_bastao = tk.Button(jogadores_e_bastoes, text="Rodar Bastões", command=rodar_batoes)
-    botao_bastao.pack()
+    botao_bastao.pack(side=tk.RIGHT)
 
     aumentar_tamanho_fonte()
-#    obter_nomes_jogadores()
-    
-    window.mainloop()
-#    re.verificar_vitoria()
 
+    window.mainloop()
+
+tab("Jogador 1", "Jogador 2")
