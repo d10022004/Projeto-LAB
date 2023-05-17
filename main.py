4236dbebb654cbd826009ae7e8689a153e54da1d
d10022004 <david.fidalgo1010@hotmail.com>
2023-05-17 15:11:48 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,4 @@
 import jogo as menu
-import tabuleiro as tab
-
 play = menu.menu()
-
-def obter_nomes_jogadores():
-    jogador1 = input("Digite o nome do Jogador 1: ")
-    jogador2 = input("Digite o nome do Jogador 2: ")
-    
-    return jogador1, jogador2
-
-nomes_jogadores = obter_nomes_jogadores()
-tab.tab(*nomes_jogadores)
+import tabuleiro as tab
+tab.tab(play)
