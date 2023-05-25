a8a0220ccc878b0acd6e7b2d9850653c5b019bbc
Carolpm28 <pinheirocarolina44@gmail.com>
2023-05-25 13:00:36 +0100

    Update regras.py

--- regras.py
+++ regras.py
@@ -13,13 +13,14 @@ def regras():
     peao = 0
 
     for j in range(1, 5):
-        bastão = random.randint(0, 2)
+        bastão = random.randint(1, 2)
         if bastão == 1:
             print("bastao", j, "-> Lado branco")
             ladobranco = ladobranco + 1
             
+            
         
-        else:
+        elif bastão == 2:
             print("bastao", j, "-> Lado de madeira")
             ladomadeira = ladomadeira + 1
                 
@@ -27,12 +28,12 @@ def regras():
         
     if ladobranco == 0:
         print("  *saiu 0 lados brancos")
-        print("  *sairam 5 lados de madeira")
+        print("  *sairam 4 lados de madeira")
         print("Mova 5 quadrados e ganha uma jogada extra\n")
         peao += 5
     elif ladobranco == 1:
         print("  *saiu 1 lado branco")
-        print("  *sairam 4 lados de madeira")
+        print("  *sairam 3 lados de madeira")
         print("Mova 1 quadrado e ganha uma jogada extra\n")
         peao += 1
     elif ladobranco == 2:
@@ -42,11 +43,12 @@ def regras():
         peao += 2
     elif ladobranco == 3:
         print("  *sairam 3 lados brancos")
-        print("  *sairam 2 lados de madeira")
+        print("  *sairam 1 lados de madeira")
         print("Mova 3 quadrados\n")
         peao += 3
     elif ladobranco == 4:
-        print("  *saiu 1 lado de madeira\n")
+        print("  *saiu 4 lados brancos")
+        print("  *saiu 0 lado de madeira\n")
         print("Mova 4 quadrados e ganha uma jogada extra")
         peao += 4
 
