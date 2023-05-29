8025c7c598e4b562657501a400cd72604de5f426
Salazers <leonardodanielsalazar2001@gmail.com>
2023-05-29 23:15:03 +0100

    Update jogo.py

--- jogo.py
+++ jogo.py
@@ -19,15 +19,6 @@ def menu():
     CINZA = (200, 200, 200)
     VERDE = (0, 255, 0)
     
-    # Render the title text and create a gradient background
-    title_font = pygame.font.Font(None, 80)
-    title_text = title_font.render("SENET", True, (255, 0 ,0))
-    title_text_rect = title_text.get_rect(center=(compri//2, larg//2 - 80))
-    gradient = pygame.Surface((title_text_rect.width, title_text_rect.height), pygame.SRCALPHA)
-    pygame.draw.rect(gradient, (0, 0, 0, 50), gradient.get_rect())
-    janela.blit(gradient, title_text_rect)
-    janela.blit(title_text, title_text_rect)
-
     def criar_botao(texto, cor_normal, cor_hover, posicao, largura, altura, acao):
         retangulo = pygame.Rect(posicao[0], posicao[1], largura, altura)
 
@@ -42,6 +33,13 @@ def menu():
         texto_surface = pygame.font.Font(None, 40).render(texto, True, PRETO)
         texto_rect = texto_surface.get_rect(center=retangulo.center)
         janela.blit(texto_surface, texto_rect)
+
+    # Render the title text and create a gradient background
+    title_font = pygame.font.Font(None, 80)
+    title_text = title_font.render("SENET", True, (255, 0 ,0))
+    title_text_rect = title_text.get_rect(center=(compri // 2, larg // 2 - 80))
+    gradient = pygame.Surface((title_text_rect.width, title_text_rect.height), pygame.SRCALPHA)
+    pygame.draw.rect(gradient, (0, 0, 0, 50), gradient.get_rect())
 
     def novojogo():
         def intrnome():
@@ -84,59 +82,22 @@ def menu():
             label_nome.pack()
             janela_nome1.mainloop()
 
-        def intrnomeind():
-            def fecharind(entrada):
-                jogadoresd['nome1'] = entrada.get()
-                jogadores.append(jogadoresd)
-                janela_nomeind.destroy()
-                jogadoresd['nome2'] = 'BOT'
-                global estado_jogo
-                estado_jogo = False
-
-            janela_op.destroy()
-            janela_nomeind = tk.Tk()
-            janela_nomeind.title("JOGADOR 1")
-            janela_nomeind.geometry("400x200")
-            label = tk.Label(janela_nomeind, text="  Introduza o seu nome:  ")
-            label.pack()
-            entrada = tk.Entry(janela_nomeind)
-            entrada.pack()
-            botao = tk.Button(janela_nomeind, text="OK", command=lambda: fecharind(entrada))
-            botao.pack()
-            label_nome = tk.Label(janela_nomeind, text="")
-            label_nome.pack()
-            janela_nomeind.mainloop()
-
+        global estado_jogo
+        estado_jogo = False
         janela_op = tk.Tk()
-        janela_op.title("Introduza uma opção")
-        janela_op.geometry("400x200")
-        labelop1 = tk.Label(janela_op, text="  Qual pretende jogar?  ")
-        labelop1.pack()
-        botao1 = tk.Button(janela_op, text="   1 VS 1   ", command=intrnome)
-        botao1.pack()
+        janela_op.title("OPÇÕES")
+        janela_op.geometry("200x200")
+        botao = tk.Button(janela_op, text="OK", command=intrnome)
+        botao.pack()
         janela_op.mainloop()
 
     def carregajogo():
-        print("CARREGA JOGO")
+        global estado_jogo
+        estado_jogo = False
 
     def regrasjogo():
-        def fechajanela():
-            janela_regras.destroy()
-
-        janela_regras = tk.Tk()
-        janela_regras.title("Regras SENET")
-        janela_regras.geometry("1280x1024")
-        frase1 = tk.Label(janela_regras, text="O jogo utiliza quatro varas de arremesso que têm a forma de meio cilindro, com a \n superfície arredondada pintada de preto e a superfície plana pintada de branco. O objetivo do \n jogo é lançar as varas e contar o número de brancos obtidos. Um resultado de nenhum é \ncontado como cinco, permitindo ao jogador avançar 5 casas e ter uma jogada extra. Se o \n resultado do lançamento for quatro ou um, o jogador avança o número de casas correspondente \n e ganha uma jogada extra. No caso de sair dois ou três, o jogador realiza um \n movimento e passa a vez ao adversário. Se não houver nenhum movimento legal disponível, o \n jogador perde a vez.\n", font=("Arial", 16))
-        frase1.pack()
-        frase2 = tk.Label(janela_regras, text="\nQuando não é possível mover nenhuma peça em qualquer direção, o jogador deve \n retroceder 5 casas. O tabuleiro possui 6 casas especiais: a Casa das Águas, onde a única \n maneira de sair é tirando 4 varas iguais em um único lançamento (com apenas uma tentativa \n por turno), ou voltando para a Casa da Segunda Vida. Há também a Casa da Beleza, que requer uma\n pontuação extra para entrar, e todas as peças precisam passar por ela para sair do \n tabuleiro. As outras três casas especiais são a Câmara dos Três Juízes, onde é necessário \n obter exatamente o número 3 para sair do tabuleiro, a Câmara dos Dois Juízes, onde é \n necessário obter exatamente o número 2, e a Casa de Heru (Hórus), onde é permitido tirar \n qualquer número acima de 1.\n", font=("Arial", 16))
-        frase2.pack()
-        frase3 = tk.Label(janela_regras, text="\nO objetivo final é ser o primeiro jogador a retirar todas as suas peças do tabuleiro.", font=("Arial", 16))
-        frase3.pack()
-        frasef = tk.Label(janela_regras, text="")
-        frasef.pack()
-        botaoregra = tk.Button(janela_regras, text="          OK          ", command=fechajanela)
-        botaoregra.pack()
-        janela_regras.mainloop()
+        global estado_jogo
+        estado_jogo = False
 
     def sair():
         global estado_jogo
@@ -149,6 +110,11 @@ def menu():
 
         if estado_jogo:
             janela.blit(imagemfundo, (0, 0))
+
+            # Blit the title text and gradient background
+            janela.blit(gradient, title_text_rect)
+            janela.blit(title_text, title_text_rect)
+
             criar_botao("NOVO JOGO", CINZA, VERDE, (250, 180), 220, 50, novojogo)
             criar_botao("CARREGA JOGO", CINZA, VERDE, (250, 240), 220, 50, carregajogo)
             criar_botao("REGRAS JOGO", CINZA, VERDE, (250, 300), 220, 50, regrasjogo)
@@ -156,12 +122,6 @@ def menu():
 
         pygame.display.update()
 
-    a = random.randint(0, 1)
-    if a == 0:
-        return jogadoresd
-    if a == 1:
-        jogadoresd['nome1'], jogadoresd['nome2'] = jogadoresd['nome2'], jogadoresd['nome1']
-        return jogadoresd
-
+    pygame.quit()
 
 menu()
